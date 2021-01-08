aaa8a49c8964e69f1d371aa6c64a1fdd3d0c1360
rmn7 <armanit.garg@gmail.com>
2021-01-09 00:06:58 +0530

    fix: rename environment to be appropriate

--- cmd/main.go
+++ cmd/main.go
@@ -53,7 +53,7 @@ func main() {
 	lastProcessedTime := time.Now()
 	timeMutex := &sync.Mutex{}
 	streamMutex := &sync.Mutex{}
-	gracePeriod, err := strconv.ParseFloat(os.Getenv("TERMINATION_GRACE_PERIOD_SECONDS"), 32)
+	gracePeriod, err := strconv.ParseFloat(os.Getenv("ALLOWED_INACTIVITY_PERIOD_SECONDS"), 32)
 
 	// TODO: do i have to do graceful shutdown or can i just kill the program?
 	go func() {
